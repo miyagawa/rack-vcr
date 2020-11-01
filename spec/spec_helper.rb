ecfcbde073216e4bf0115b1eff69cceb379b64f9
Richard Macklin <richard.github@nrm.com>
2020-10-31 21:53:59 -0700

    Enforce minimum test coverage in simplecov configuration
    Let's prevent our test coverage from dropping

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 if ENV['MEASURE_COVERAGE'] == 'true'
   require 'simplecov'
-  SimpleCov.start
+  SimpleCov.start do
+    minimum_coverage 100
+  end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
